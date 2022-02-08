41bba465e0071c5f50910d08cd92146646149892
lynzoie <69117554+lynzoie@users.noreply.github.com>
2022-02-08 18:32:23 -0500

    working Bluetooth, fix GPS

--- Nodens_BT.ino
+++ Nodens_BT.ino
@@ -2,7 +2,6 @@
 #include <SoftwareSerial.h>
 #define USE_ARDUINO_INTERRUPTS true
 #include <PulseSensorPlayground.h>
-
 SoftwareSerial HM10(2,3);
 char appData;  
 String inData = "";
@@ -16,13 +15,21 @@ Adafruit_MPU6050 mpu;
 // Heart Rate Sensor variables
 int PulseWire = 0;                  // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;
+int HR_Val;                         // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 400;                // Determine which Signal to "count as a beat", and which to ingore.
 PulseSensorPlayground pulseSensor;  // Create PulseSensorPlayground object
 
-int HR_Val;                         // holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 550;                // Determine which Signal to "count as a beat", and which to ingore.
+// GPS Module variables
+#include <TinyGPS++.h>
+static const int RXPin = 6, TXPin = 5;
+// The TinyGPS++ object
+TinyGPSPlus gps;
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
   Serial.begin(9600);
+  
   //////////// HEART RATE SENSOR SETUP //////////////
   pulseSensor.analogInput(PulseWire);
   pulseSensor.blinkOnPulse(LED13);   // pin that will blink to your heartbeat!
@@ -30,11 +37,9 @@ void setup() {
   pulseSensor.begin();
 
   ///////////////// BLUETOOTH SETUP /////////////////
-  // Serial.println("HM10 serial started at 9600");
   HM10.begin(9600);
-
+  HM10.write("Connection Established");   // indicate BT-Phone connection has been made
   ////////// ACCELEROMETER/GYROSCOPE SETUP //////////
-  //Serial.begin(115200);
   // find MPU6050
   if (!mpu.begin()) {
     //Serial.println("Failed to find MPU6050 chip");
@@ -42,41 +47,13 @@ void setup() {
       delay(10);
     }
   }
-  // set accelerometer range to +-8G
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  // set gyro range to +- 500 deg/s
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  // set filter bandwidth to 21 Hz
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);   // set accelerometer range to +-8G
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);        // set gyro range to +- 500 deg/s
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);     // set filter bandwidth to 21 Hz
+  
+  ///////////////// GPS SETUP ///////////////////////
+  ss.begin(9600);
 }
-
-/*
-void BT_Mod(val) {
-  HM10.listen();  // listen the HM10 port
-  while (HM10.available() > 0) {   // if HM10 sends something then read
-    appData = HM10.read();
-    inData = String(appData);  // save the data in string format
-    //Serial.write(appData);
-  }
-    
-  if (Serial.available()) {           // Read user input if available.
-    delay(10);
-    HM10.write(Serial.read());
-  }
-  if ( inData == "F") {
-    Serial.println("LED OFF");
-    digitalWrite(12, LOW);
-    //digitalWrite(13, LOW); // switch OFF LED
-    delay(1000);
-  }
-  if ( inData == "N") {
-    Serial.println("LED ON");
-    digitalWrite(12, HIGH);
-    delay(1000);
-    digitalWrite(12, HIGH);
-    delay(1000);
-  }
-}*/
 
 void Send_Data(String String_Message, SoftwareSerial HM10) {      // Converts strings to char arrays so that we can transmit data via Bluetooth
   char* char_message = (char*) malloc(sizeof(char)*String_Message.length()+1);
@@ -85,56 +62,52 @@ void Send_Data(String String_Message, SoftwareSerial HM10) {      // Converts st
   free(char_message);
   
 }
-String HR_Str;
-String AG_Str[6];
-
+String Data_Packet;
+bool flag = true;
 void loop() {
-  
-  // HR_Val = analogRead(PulseSensorPurplePin); // 10-bit number, convert to string
   HR_Val = pulseSensor.getBeatsPerMinute();
-  if (pulseSensor.sawStartOfBeat()) {
-    Serial.println(HR_Val);
-  }
+
   // get values of Accelerometer/Gyroscope
   sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp); 
+  mpu.getEvent(&a, &g, &temp);  
 
-  // Display data on serial plotter
-//  Serial.print(HR_Val);
-//  Serial.print(a.acceleration.x); 
-//  Serial.print(",");
-//  Serial.print(a.acceleration.y);
-//  Serial.print(",");
-//  Serial.print(a.acceleration.z);
-//  Serial.print(",");
-//  Serial.print(g.gyro.x);
-//  Serial.print(",");
-//  Serial.print(g.gyro.y);
-//  Serial.print(",");
-//  Serial.print(g.gyro.z);  
+//  ss.listen();    // listen to GPS port
+//  // creating GPS data
+//  if (ss.available() > 0){
+//     gps.encode(ss.read());
+//     if (gps.location.isUpdated()){
+//       Serial.print("Latitude= "); 
+//       Serial.print(gps.location.lat(), 6);
+//       Serial.print(" Longitude= "); 
+//       Serial.println(gps.location.lng(), 6);
+//     }
+//   }
+
+  // setup Bluetooth to listen to request to send data
+  HM10.listen();  // listen to Bluetooth port
+  while (HM10.available() > 0) {   // if HM10 sends something then read
+    appData = HM10.read();
+    inData = String(appData);  // save the data in string format
+    //Serial.write(appData);
+  }
+  if (Serial.available()) {           // Read user input if available.
+    delay(10);
+    HM10.write(Serial.read());
+  }
+  if ( inData == "S") {     // send packets
+    // Creating data packets to send to Bluetooth module
+    Data_Packet = "HR" + String(HR_Val) + "AX" + String(a.acceleration.x) + "AY" + String(a.acceleration.y) + "AZ" + String(a.acceleration.z) + "GX" + String(g.gyro.x) + "GY" + String(g.gyro.y) + "GZ" + String(g.gyro.z) + "LA" + String(gps.location.lat()) + "LO" + String(gps.location.lng());
+    
+    // Sending heart-rate, accelerometer, and gyroscope data
+    if (pulseSensor.sawStartOfBeat()) {
+      Serial.println(HR_Val);
+      Send_Data(Data_Packet, HM10);
+    }
+  }
   
-  // Creating data packets to send to Bluetooth module
-  HR_Str    = "HR"  + String(HR_Val);
-  AG_Str[0] = "AX" + String(a.acceleration.x);
-  AG_Str[1] = "AY" + String(a.acceleration.y);
-  AG_Str[2] = "AZ" + String(a.acceleration.z);
-  AG_Str[3] = "GX" + String(g.gyro.x);
-  AG_Str[4] = "GY" + String(g.gyro.y);
-  AG_Str[5] = "GZ" + String(g.gyro.z);
-  // Sending heart-rate, accelerometer, and gyroscope data
-  Send_Data(HR_Str, HM10);
-  Send_Data(AG_Str[0],HM10);
-  Send_Data(AG_Str[1],HM10);
-  Send_Data(AG_Str[2],HM10);
-  Send_Data(AG_Str[3],HM10);
-  Send_Data(AG_Str[4],HM10);
-  Send_Data(AG_Str[5],HM10);
+  // Send_Data(GPS.latitude);
+  // Send_Data(GPS.longitude);
   
-  // Send_Data(GPS.x);
-  
-  // Send_Data(GPS.y);
-  
-  // Send_Data(GPS.z);
-  delay(10);
+  delay(20);
   
 }
