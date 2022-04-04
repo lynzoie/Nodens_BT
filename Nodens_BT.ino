7033014ca163b7dc1f40b1f5cc306729a360b87d
Lynzie Romero <lynzieromerobugay@gmail.com>
2022-04-03 20:07:32 -0400

    removed acc/gyro

--- Nodens_BT.ino
+++ Nodens_BT.ino
@@ -7,10 +7,10 @@ char appData;
 String inData = "";
 
 // Accelerometer & Gyroscope libraries and variables
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_MPU6050.h>
-Adafruit_MPU6050 mpu;
+//#include <Wire.h>
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_MPU6050.h>
+//Adafruit_MPU6050 mpu;
 
 // Heart Rate Sensor variables
 int PulseWire = 0;                  // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
@@ -41,15 +41,15 @@ void setup() {
   HM10.write("Connection Established");   // indicate BT-Phone connection has been made
   
   ////////// ACCELEROMETER/GYROSCOPE SETUP //////////
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);   // set accelerometer range to +-8G
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);        // set gyro range to +- 500 deg/s
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);     // set filter bandwidth to 21 Hz
+//  if (!mpu.begin()) {
+//    Serial.println("Failed to find MPU6050 chip");
+//    while (1) {
+//      delay(10);
+//    }
+//  }
+//  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);   // set accelerometer range to +-8G
+//  mpu.setGyroRange(MPU6050_RANGE_500_DEG);        // set gyro range to +- 500 deg/s
+//  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);     // set filter bandwidth to 21 Hz
   
   ///////////////// GPS SETUP ///////////////////////
   ss.begin(9600);
@@ -85,8 +85,8 @@ void loop() {
     HR_Val = pulseSensor.getBeatsPerMinute();
  
     // get values of Accelerometer/Gyroscope
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);  
+//    sensors_event_t a, g, temp;
+//    mpu.getEvent(&a, &g, &temp);  
 
     // get GPS data
 //    ss.listen(); 
@@ -104,7 +104,7 @@ void loop() {
 //    Serial.println(gps_lng,7);
     if (gps_lat != 0 && gps_lng != 0) {     // if we get valid GPS data, get ready to send data packet via BT
       // Creating data packets to send to Bluetooth module
-      String Data_Packet = "FFFFHR" + String(HR_Val) + "AX" + String(a.acceleration.x) + "AY" + String(a.acceleration.y) + "AZ" + String(a.acceleration.z) + "GX" + String(g.gyro.x) + "GY" + String(g.gyro.y) + "GZ" + String(g.gyro.z) + "LA" + String(gps_lat,7) + "LO" + String(gps_lng,7) + "DDDD";
+      String Data_Packet = "FFFFHR" + String(HR_Val) + "AX" + String(0) + "AY" + String(0) + "AZ" + String(0) + "GX" + String(0) + "GY" + String(0) + "GZ" + String(0) + "LA" + String(gps_lat,7) + "LO" + String(gps_lng,7) + "DDDD";
       HM10.listen();                        // listen to BT again so we can send data ports
 //      Send_Data(Data_Packet, HM10);         // assume we're getting heart rate for now, use below if statement
       if (pulseSensor.sawStartOfBeat()) {   // send packets only if we get valid heart beat
